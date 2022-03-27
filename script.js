3aead4edcaa8bd20c4be9c4d770a614a442fa438
tony-samson <93037718+tony-samson@users.noreply.github.com>
2022-03-27 15:52:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
